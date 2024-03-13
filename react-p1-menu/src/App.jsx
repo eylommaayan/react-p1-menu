6172f788ea90d86e9b36bfa6d93cc5a15cdae3e4
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-03-13 23:29:39 +0200

    Update App.jsx

--- react-p1-menu/src/App.jsx
+++ react-p1-menu/src/App.jsx
@@ -1,13 +1,26 @@
-import './index.css';
-import mushroomsImage from './assets/mushrooms.jpg'; // Corrected import path
+import "./index.css";
+import mushroomsImage from "./assets/mushrooms.jpg"; // Corrected import path
 
 function Pizza() {
   return (
     <>
       <img src={mushroomsImage} alt="Mushrooms" />
       <h2>Pizza</h2>
-    </> 
+    </>
   );
+}
+
+function Header() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  // if (hour >= openHour && hour <= closeHour) alert("כרגע אנחנו פתוחים");
+  // else alert("מצטערים סגרנו ");
+
+  return <h1>Fast Reacr Pizza </h1>;
 }
 
 function App() {
@@ -18,6 +31,7 @@ function App() {
       <Pizza />
       <Pizza />
       <Pizza />
+      <Header />
     </>
   );
 }
