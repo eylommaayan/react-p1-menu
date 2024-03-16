a895eabef79564e0af84cff45d6f9ec436c29e43
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-03-16 22:39:25 +0200

    הוספת רשימה של מידע על הפיצות לקובץ הראשי

--- react-p1-menu/src/App.jsx
+++ react-p1-menu/src/App.jsx
@@ -1,5 +1,124 @@
 import "./index.css";
-import mushroomsImage from "./assets/mushrooms.jpg"; // Corrected import path
+import React from "react";
+import { ReactDOM } from "react";
+
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+
+
+
+function App() {
+  return (
+    <main className='menu'>
+      <Header/>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Footer />
+    </main>
+  );
+}
+
+
+
+function Header() {
+  return (
+    <header  className="header">
+      <h1>תפריט פיצה לאונרדו</h1>
+    </header>
+  );
+}
+
+
+function Menu() {
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const numPizzas = pizzas.length;
+
+  return (
+    <main className="menu">
+      <h2>Our menu</h2>
+
+      {numPizzas > 0 ? (
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
+      )}
+
+      {/* <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mushrooms"
+        price={12}
+        photoName="pizzas/funghi.jpg"
+      /> */}
+    </main>
+  );
+}
+
+
+
+
 
 function Pizza() {
   return (
@@ -10,32 +129,40 @@ function Pizza() {
   );
 }
 
-function Header() {
 
 
+function Footer() {
   const hour = new Date().getHours();
   const openHour = 12;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
 
-  // if (hour >= openHour && hour <= closeHour) alert("כרגע אנחנו פתוחים");
-  // else alert("מצטערים סגרנו ");
+  // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
+  // else alert("Sorry we're closed");
 
-  return <h1>Fast Reacr Pizza </h1>;
+  // if (!isOpen) return <p>CLOSED</p>;
 
-}
-
-function App() {
   return (
-    <>
-     <Header/>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Header />
-    </>
+    <footer className="footer">
+      {isOpen ? (
+        <Order closeHour={closeHour} openHour={openHour} />
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
+      )}
+    </footer>
   );
+
+  // return React.createElement("footer", null, "We're currently open!");
 }
+
+
+// style-  דרך1 - אפשר לרשום ישירות אחרי התג
+// דדרך 2 -  שאפשר ליצור מתשנה בוא נגדיר עיצוב 
+// דרך 3 - ליצור קובץ עיצוב נפרד
+
+
 
 export default App;
